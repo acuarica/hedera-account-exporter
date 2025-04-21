670331b0bfe81d256d3f5cf07a9baaa7c4921d9c
Luis Mastrangelo <luis@swirldslabs.com>
2025-04-21 21:26:13 +0200

    Add html templating

--- bin/hedera-account-exporter.js
+++ bin/hedera-account-exporter.js
@@ -2,6 +2,7 @@
 
 import { program, Option } from 'commander';
 import figlet from 'figlet';
+import Handlebars from 'handlebars';
 
 import packageJson from '../package.json' with { type: 'json' }
 import { Forex } from "../src/exchange-rate.js";
@@ -9,6 +10,23 @@ import { MirrorNodeClient } from "../src/mirror-node-client.js";
 import { csv } from '../src/csv.js';
 import { fmt } from '../src/fmt.js';
 import { getTransfers } from "../src/index.js";
+import { readFileSync, writeFileSync } from 'fs';
+
+const symbols = {
+    HBAR: 'ℏ',
+    USD: '$',
+    CHF: '₣',
+};
+
+const diffFormat = new Intl.NumberFormat('en-CH', {
+    minimumFractionDigits: 4,
+    maximumFractionDigits: 4,
+});
+
+const currencyFormat = new Intl.NumberFormat('en-CH', {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+});
 
 /**
  * 
@@ -32,17 +50,18 @@ async function main(accounts, currencies) {
 const cmd = program
     .description(packageJson.description)
     .version(packageJson.version)
-    .addOption(new Option('-c, --currencies <symbols...>', 'specify currency exchange rates').choices(['USD', 'CHF']))
-    .addOption(new Option('--summary', 'display total summary'))
-    .addOption(new Option('--hashscan', 'display transaction IDs as links to Hashscan').conflicts('summary'))
+    .addOption(new Option('-c, --currencies <symbols...>', 'specify currency exchange rates').choices(['USD', 'CHF']).default([], 'only HBAR'))
+    .addOption(new Option('-s, --summary', 'display total summary'))
+    .addOption(new Option('--html', 'display report as HTML'))
+    .addOption(new Option('--hashscan', 'display transaction IDs as links to Hashscan (only applicable when output is CSV)').conflicts('summary'))
     .argument('<accounts...>')
     .showHelpAfterError()
     .parse();
+const opts = cmd.opts();
 
-const { currencies = [], hashscan, summary } = cmd.opts();
-const tss = await main(cmd.args, currencies);
+const tss = await main(cmd.args, opts.currencies);
 
-if (summary) {
+if (opts.summary) {
     console.info(figlet.textSync('Hedera Account Exporter'));
     console.table(Object.fromEntries(Object.entries(tss).map(
         ([account, { total, balance }]) => [
@@ -55,18 +74,46 @@ if (summary) {
     )));
 } else {
     const table = Object.values(tss).flatMap(ts => ts.transfers).map(t => ({
+        // ...t,
+        date: t.date,
         Date: t.date.toLocaleDateString('en-CH', { timeZone: 'Europe/Zurich' }),
         Account: t.account,
-        'Transaction ID': (hashscan ? 'https://hashscan.io/mainnet/transaction/' : '') + t.transactionId,
-        'Amount (HBAR)': fmt(t.amount),
-        'Accum (HBAR)': fmt(t.accum),
-        'Balance at (HBAR)': fmt(t.balanceAt),
-        'Diff (HBAR)': fmt(t.diff),
-        Remarks: t.remarks,
-        ...Object.fromEntries(/**@type{string[]}*/(currencies).map(c => [
-            `HBAR-${c} Rate`,
-            /**@type{import('../src/hbar.js').Hbar}*/(t)[`HBAR-${c}`
-            ]])),
+        'Transaction ID': (opts.hashscan ? 'https://hashscan.io/mainnet/transaction/' : '') + t.transactionId,
+        transactionId: t.transactionId,
+        amount: currencyFormat.format(fmt(t.amount)),
+        accum: currencyFormat.format(fmt(t.accum)),
+        balanceAt: currencyFormat.format(fmt(t.balanceAt)),
+        diff: diffFormat.format(fmt(t.diff)),
+        remarks: t.remarks,
+        ...Object.fromEntries(/**@type{string[]}*/(opts.currencies).flatMap(c => [
+            [`HBAR-${c}-Rate`, t.values[c].rate],
+            [`HBAR-${c}-Amount`, t.values[c].amount],
+        ])),
+        values: Object.fromEntries(
+            Object
+                .entries(t.values)
+                .map(([currency, { rate, amount }]) => [
+                    currency, {
+                        rate: rate.toFixed(4),
+                        amount: currencyFormat.format(amount.toString()),
+                    }
+                ])
+        ),
     }));
-    process.stdout.write(csv(table));
+
+    if (opts.html) {
+
+        // {{#each ../currencies}}
+        // <td align="right">{{ lookup ../this this }}</td>
+        // <td align="right">{{ lookup ../this this }}</td>
+        // {{/each}}
+
+        const groups = Object.groupBy(table, ({ date }) => date.getFullYear() + '-' + (date.getMonth() + 1));
+        console.log(groups);
+        const html = readFileSync('./template.html', 'utf-8');
+        const template = Handlebars.compile(html);
+        writeFileSync('output.html', template({ groups: groups, currencies: opts.currencies, symbols }));
+    } else {
+        process.stdout.write(csv(table));
+    }
 }
